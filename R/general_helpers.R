3b699cc47507f141a82e06827d0b848e38c6687a
Joshua Born <joshua.born@gmail.com>
2022-04-12 21:24:42 -0400

    Create helper for horizontally combining huxtables; fix issues with extant huxtable helpers

--- R/general_helpers.R
+++ R/general_helpers.R
@@ -207,6 +207,24 @@ estimate_mentions_in_subdomains <- function(row_estimation_function, indices) {
   ))
 }
 
+theme_article_extra <- function(ht) {
+  header_rows <- which(header_rows(ht))
+  header_cols <- which(header_cols(ht))
+  body_rows <- which(!header_rows(ht))
+  body_cols <- which(!header_cols(ht))
+  ht |>
+    theme_article() |>
+    set_align(everywhere, body_cols, 'center') |>
+    set_tb_padding(header_rows, everywhere, 0) |>
+    set_all_padding(body_rows, everywhere, 3) |>
+    set_bold(everywhere, header_cols, FALSE) |>
+    set_left_padding(
+      everywhere,
+      seq(min(body_cols), ncol(ht), 2),
+      16
+    )
+}
+
 style_totals_and_percentages <- function(dt) {
   dt |>
   mutate_vars('total', \(x) x / 10e5) |>
@@ -231,58 +249,93 @@ style_totals_and_percentages <- function(dt) {
   merge_cells(1, 2:3) |>
   merge_cells(1, 4:5) |>
   set_header_rows(1:2, TRUE) |>
-  theme_article() |>
   set_header_cols(1, TRUE) |>
-  set_align(everywhere, 2:5, 'center') |>
-  set_all_padding(everywhere, everywhere, 4) |>
-  set_left_padding(everywhere, c(2, 4), 12) |>
-  set_tb_padding(1:2, everywhere, 0)
+  theme_article_extra()
 }
 
 combine_huxtables_vertically <- function(label1, ht1, label2, ht2) {
   if (!all.equal(ht1[header_rows(ht1),], ht2[header_rows(ht2),])) {
     stop('huxtables do not have identical headers and cannot be combined vertically')
-  } else {
-    ht <- rbind(
-      ht1,
-      ht2[!header_rows(ht2), ]
-    )
-    body_rows1 <- which(!header_rows(ht1))
-    body_rows2 <- which(!header_rows(ht2))
-    body_start_index1 <- body_rows1[1]
-    body_start_index2 <- body_rows1[-1] + 1
-    label_column <- rep('', nrow(ht))
-    label_column[body_start_index1] <- label1
-    label_column[body_start_index2] <- label2
-    header_column_index <- 1
-    old_header_columns <- names(header_cols(ht))[header_cols(ht)]
-    insert_column(ht, label_column) |>
-      set_header_cols(header_column_index, TRUE) |>
-      style_header_cols(valign = 'middle') |>
-      set_rowspan(
-        body_start_index1,
-        header_column_index,
-        length(body_rows1)
-      ) |>
-      set_rowspan(
-        body_start_index2,
-        header_column_index,
-        length(body_rows2)
-      ) |>
-      theme_article() |>
-      set_bold(everywhere, old_header_columns, FALSE) |>
-      set_left_padding(everywhere, header_column_index, 4) |>
-      set_right_padding(everywhere, final(1), 4) |>
-      set_right_padding(everywhere, header_column_index, 12) |>
-      set_tb_padding(everywhere, header_column_index, 0) |>
-      set_bottom_border(body_rows1[-1], everywhere)
   }
+  ht <- rbind(
+    ht1,
+    ht2[!header_rows(ht2), ]
+  )
+  body_rows1 <- which(!header_rows(ht1))
+  body_rows2 <- which(!header_rows(ht2))
+  body_start_index1 <- min(body_rows1)
+  body_start_index2 <- max(body_rows1) + 1
+  label_column <- rep('', nrow(ht))
+  label_column[body_start_index1] <- label1
+  label_column[body_start_index2] <- label2
+  header_column_index <- 1
+  old_header_columns <- names(header_cols(ht))[header_cols(ht)]
+  insert_column(ht, label_column) |>
+    set_header_cols(header_column_index, TRUE) |>
+    style_header_cols(valign = 'middle') |>
+    set_rowspan(
+      body_start_index1,
+      header_column_index,
+      length(body_rows1)
+    ) |>
+    set_rowspan(
+      body_start_index2,
+      header_column_index,
+      length(body_rows2)
+    ) |>
+    theme_article_extra() |>
+    set_bold(everywhere, header_column_index, TRUE) |>
+    set_right_padding(everywhere, header_column_index, 12) |>
+    set_top_border(body_start_index2, everywhere)
 }
 
 style_and_vertically_combine_totals_and_percentages <- function(
   label1, estimate1, label2, estimate2
 ) {
   combine_huxtables_vertically(
+    label1,
+    style_totals_and_percentages(estimate1),
+    label2,
+    style_totals_and_percentages(estimate2)
+  )
+}
+
+combine_huxtables_horizontally <- function(label1, ht1, label2, ht2) {
+  if (!all.equal(ht2[, header_cols(ht2)], ht1[, header_cols(ht1)])) {
+    stop('huxtables do not have identical header columns and cannot be combined horizontally')
+  }
+  body_cols1 <- which(!header_cols(ht1))
+  body_cols2 <- which(!header_cols(ht2))
+  body_start_index1 <- min(body_cols1)
+  body_start_index2 <- max(body_cols1) + 1
+  superheader_row <- rep('', ncol(ht1) + sum(!header_cols(ht2)))
+  superheader_row[body_start_index1] <- label1
+  superheader_row[body_start_index2] <- label2
+  cbind(
+    ht1,
+    ht2[, which(!header_cols(ht2))]
+  ) |>
+    insert_row(superheader_row, after = 0) |>
+    set_header_rows(1, TRUE) |>
+    set_colspan(
+      1,
+      body_start_index1,
+      length(body_cols1)
+    ) |>
+    set_colspan(
+      1,
+      body_start_index2,
+      length(body_cols2)
+    ) |>
+    theme_article_extra() |>
+    set_left_border(everywhere, body_start_index1) |>
+    set_left_border(everywhere, body_start_index2)
+}
+
+style_and_horizontally_combine_totals_and_percentages <- function(
+  label1, estimate1, label2, estimate2
+) {
+  combine_huxtables_horizontally(
     label1,
     style_totals_and_percentages(estimate1),
     label2,
