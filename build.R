840c91bfa8b64953a01a88ed466c593c8cff1083
Joshua Born <joshua.born@gmail.com>
2022-04-27 18:51:51 -0400

    Include imported articles at root of content/

--- build.R
+++ build.R
@@ -1,7 +1,25 @@
+submodule_path <- file.path('content', 'NSFG')
+submodule_inclusion_path <- file.path(submodule_path, 'reports')
+
 blogdown::build_site(
+  run_hugo = FALSE,
   build_rmd = function(x) {
-    not_NSFG <- x[grep('content/NSFG/', x, invert = TRUE)]
-    NSFG_reports <- x[grep('content/NSFG/reports', x)]
+    not_NSFG <- x[grep(submodule_path, x, invert = TRUE)]
+    NSFG_reports <- x[grep(submodule_inclusion_path, x)]
     c(not_NSFG, NSFG_reports)
   }
+)
+
+filepaths <- list.files(submodule_inclusion_path, "\\.html$", full.names = TRUE)
+
+blogdown::build_dir(submodule_inclusion_path, force = TRUE)
+
+file.rename(
+  filepaths,
+  gsub(submodule_inclusion_path, 'content', filepaths)
+)
+
+blogdown::build_site(
+  run_hugo = TRUE,
+  build_rmd = FALSE
 )
