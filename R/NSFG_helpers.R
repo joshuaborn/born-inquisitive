085b632f5dc5775ece007c25f212ee751afe0760
Joshua Born <joshua.born@gmail.com>
2022-06-09 17:42:05 -0400

    Explore CMSTRTMC, CMENDMC, CMDATBGN, and start pivot table with MONSX

--- R/NSFG_helpers.R
+++ R/NSFG_helpers.R
@@ -81,10 +81,12 @@ get_NSFG_format_indices <- function(format, skip = NULL, sex = 'fem') {
   }
 }
 
-factorize_NSFG_variable <- function(x, name, sex = 'fem', fill_na = TRUE) {
-  formats_table <- get_NSFG_formats_for_sex(sex)
+factorize_NSFG_variable <- function(x, name, sex = 'fem', fill_na = TRUE, formats_table = NULL) {
+  if (is.null(formats_table)) {
+    formats_table <- get_NSFG_formats_for_sex(sex)
+  }
   if (fill_na) {
-    nafill_value <- max(formats_table[format_name == name, factor_value]) + 1
+    nafill_value <- max(formats_table[format_name == name, as.integer(factor_value)]) + 1
     factor(
       nafill(x, fill = nafill_value),
       levels = c(
@@ -200,4 +202,28 @@ estimate_and_combine_NSFG_totals_and_percentages <- function(f, digits = 2) {
     ),
     digits = digits
   )
+}
+
+to_century_month <- function(x) {
+  x_int <- as.integer(x)
+  months <- factor(
+    x_int %% 12,
+    levels = 0:11,
+    labels = c('Dec', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov')
+  )
+  years <- floor((x_int - 1) / 12) + 1900
+  fifelse(
+    x_int > 9000,
+    fcase(
+      x_int == 9997, 'Not ascertained',
+      x_int == 9998, 'Refused',
+      x_int == 9999, "Don't know"
+    ),
+    paste(as.character(months), years)
+  )
+}
+
+names_to_century_month <- function(x) {
+  names(x) <- to_century_month(names(x))
+  x
 }
