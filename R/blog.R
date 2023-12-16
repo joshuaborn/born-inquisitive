5018a1f5371b95544bc6a3880a0920fe2161beac
Joshua Born <joshua.born@gmail.com>
2023-12-16 11:50:51 -0500

    Fix blog helper functions

--- R/blog.R
+++ R/blog.R
@@ -1,37 +1,29 @@
 library(here)
+library(blogdown)
 
 i_am('R/blog.R')
 
+site_dir <- here('blog')
 
 build_hugo <- function() {
-  setwd(here('blog/'))
-  blogdown::build_site(
+  build_site(
     build_rmd = FALSE,
     run_hugo = TRUE,
-    args = c(paste('-s', here('blog/')))
+    args = c(paste('-s', site_dir))
   )
-  setwd(here())
 }
 
-
-build <- function() {
-  setwd(here('blog/'))
-  blogdown::build_site(
+build_blog <- function() {
+  setwd(site_dir)
+  build_site(
     build_rmd = TRUE,
     run_hugo = TRUE,
-    args = c(paste('-s', here('blog/')))
+    args = c(paste('-s', site_dir))
   )
   setwd(here())
 }
 
-
 # Preview in web browser
-preview <- function() {
-  blogdown::serve_site()
-}
-
-
-# Shut down
-shutdown <- function() {
-  blogdown::stop_server()
+serve_blog <- function() {
+  serve_site(.site_dir = site_dir)
 }
