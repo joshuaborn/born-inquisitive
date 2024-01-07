41038af8e6bfa11c7dcd467c1d943bd941f1afdd
Joshua Born <joshua.born@gmail.com>
2024-01-07 08:08:50 -0500

    Only accept replicate weight design objects of class svyrep.design

--- R/adjust_weights_by_ratio.R
+++ R/adjust_weights_by_ratio.R
@@ -1,1 +1,16 @@
+library(survey)
 
+#' Adjust weights in survey design object with replicate weights
+#'
+#' `adjust_weights_by_ratio()` increases the weights of all observations that
+#' match a specified predicate in a design object created by `survey` or `srvyr`
+#' in order for an estimate to match a target total. The function only works
+#' with survey design objects specified with replicate weights.
+#'
+#' @param svy A svyrep.design object as created by `survey` or `srvyr` packages
+
+adjust_weights_by_ratio <- function(svy) {
+  if (!any(class(svy) == 'svyrep.design')) {
+    stop('Parameter svy must be of class svyrep.design')
+  }
+}
--- tests/testthat/test-adjust_weights_by_ratio.R
+++ tests/testthat/test-adjust_weights_by_ratio.R
@@ -1,3 +1,34 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+get_simple_test_data <- function() {
+  simple_df <- data.frame(
+    weight = c(1500, 750, 1250, 1100, 900, 1000, 1050, 950),
+    stratum = c(1, 1, 2, 2, 3, 3, 4, 4),
+    psu = c(1, 2, 1, 2, 1, 2, 1, 2)
+  )
+
+  simple_svy <- svydesign(
+    data = simple_df,
+    ids = ~psu,
+    strata = ~stratum,
+    weights = ~weight,
+    nest = TRUE
+  )
+
+  simple_rep_svy <- as.svrepdesign(
+    design = simple_svy,
+    type = 'BRR'
+  )
+
+  list(
+    df = simple_df,
+    svy = simple_svy,
+    rep_svy = simple_rep_svy
+  )
+}
+
+
+test_that('only svyrep.design objects are accepted', {
+  test_data <- get_simple_test_data()
+  expect_error(adjust_weights_by_ratio(test_data$df), 'svyrep.design')
+  expect_error(adjust_weights_by_ratio(test_data$svy), 'svyrep.design')
+  expect_no_error(adjust_weights_by_ratio(test_data$rep_svy))
 })
